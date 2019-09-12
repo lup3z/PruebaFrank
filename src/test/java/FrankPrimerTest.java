89d13c9759fc455957d5a924d15a6bcefe0da0d7
Lucia Aznar <laznar@kiusys.com>
2019-09-12 16:47:20 -0300

    Verificacion de elemnetos

--- src/test/java/FrankPrimerTest.java
+++ src/test/java/FrankPrimerTest.java
@@ -16,10 +16,23 @@ public static ChromeDriver driver;
         //aplico el uso en una URL especifica
         driver.get("http://songs-by-sinatra.herokuapp.com/");
 
-    //Paso 2: empiezo a definir casos de prueba
-        //caso a)
-        //caso b)
-        //caso c)
+    //Paso 2: empieso a definir casos de prueba
+        //caso a) elementos de pantalla
+        // titulo de la pagina: //title[text()="Songs By Sinatra"]
+        // una solapa: //a[@href="/about"]
+        // imagen: //img[@src="/images/sinatra.jpg"]
+        
+        
+        WebElement titulo = driver.findElement(By.xpath("//title[text()='Songs By Sinatra']"));
+        titulo.isDisplayed();
+        
+        WebElement solapa = driver.findElement(By.xpath("//a[@href='/about']"));
+        solapa.isDisplayed();
+        
+        WebElement imagen = driver.findElement(By.xpath("//img[@src='/images/sinatra.jpg']"));
+        imagen.isDisplayed();
+        
+        System.out.println("El test corrio bien");
 
 }
 
